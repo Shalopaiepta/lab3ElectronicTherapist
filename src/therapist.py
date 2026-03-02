8f563bf3697517bce96c1bc3b3fed6b8a4bdc5e0
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-02 22:46:24 +0700

    Рефакторю therapist.py, добавляю первую базу вопросов

--- src/therapist.py
+++ src/therapist.py
@@ -1,8 +1,10 @@
 class Therapist:
 
     def __init__(self):
-        # todo: реализовать инициализацию базы вопросов
-        pass
+        self.questions = [
+            "У вас есть температура?",
+            "Есть ли кашель?"
+        ]
     def ask_question(self, index: int) -> str:
         # todo: реализовать метод ask_question позднее до конца
         return "У вас есть температура?"

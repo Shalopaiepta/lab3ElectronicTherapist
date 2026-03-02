ef84365325ccc2386182088f156621aaea2211ca
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-02 22:28:03 +0700

    Рефакторю test_therapist: Добавляю тест для вопроса электронного терапевта - test_ask_question_returns_strings

--- tests/test_therapist.py
+++ tests/test_therapist.py
@@ -7,6 +7,11 @@ class TestTherapist(unittest.TestCase):
     def test_therapist_class_creation(self):
         therapist = Therapist()
         self.assertIsNotNone(therapist)
+    def test_ask_question_returns_string(self):
+        therapist = Therapist()
+        result = therapist.ask_question(0)
+        self.assertIsInstance(result, str)
+        self.assertGreater(len(result), 0)
 
 
 if __name__ == '__main__':

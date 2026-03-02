b7a6fdcaa30c3608e0cbc3515394f29a97630104
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-02 22:53:00 +0700

    Добавляю новый метод: get_diagnosis. Теперь Therapist может поставить диагноз

--- src/therapist.py
+++ src/therapist.py
@@ -12,3 +12,8 @@ class Therapist:
         if index == 1:
             return self.questions[1]
         return "Вопрос не найден"
+    def get_diagnosis(self, answers: list) -> str:
+        # todo: реализовать метод get_diagnosis позднее до конца
+        if len(answers) > 0 and answers[0] == True:
+            return "Возможна простуда"
+        return "Здоровы"

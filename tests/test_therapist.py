8292436b5fe02febb522dbe1fbccbfb9d44d39d3
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-02 22:50:52 +0700

    Добавляю новый тест -test_get_diagnosis_returns_string. Проверяется,ставится ли диагноз

--- tests/test_therapist.py
+++ tests/test_therapist.py
@@ -17,6 +17,12 @@ class TestTherapist(unittest.TestCase):
         result = therapist.ask_question(1)
         self.assertIsInstance(result, str)
         self.assertGreater(len(result), 0)
+    def test_get_diagnosis_returns_string(self):
+        therapist = Therapist()
+        answers = [True, False]
+        result = therapist.get_diagnosis(answers)
+        self.assertIsInstance(result, str)
+        self.assertGreater(len(result), 0)
 
 
 if __name__ == '__main__':

c2e81b1d306ee8d980f830e0e208966c8bcb2cca
Shalopaiepta <rkolesnikov2004@mail.ru>
2026-03-02 22:39:08 +0700

    рефакторю класс Therapist - добавляю функцию ask_question. Это заглушка, возвращает одну и ту же строчку

--- src/therapist.py
+++ src/therapist.py
@@ -3,3 +3,6 @@ class Therapist:
     def __init__(self):
         # todo: реализовать инициализацию базы вопросов
         pass
+    def ask_question(self, index: int) -> str:
+        # todo: реализовать метод ask_question позднее до конца
+        return "У вас есть температура?"
